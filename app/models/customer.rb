8bba99b6c5656d8db8f9b289cc40f35726b2a044
tony-dorfmeister <tony.dorfmeister@me.com>
2018-01-11 16:45:31 +0100

    implemented totalSumOfAllOrders

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
   has_many :orders
+
+  def sumOfAllOrders
+  	sum = 0
+  	orders.each do |order|
+  		sum += order.totalSum
+  	end
+  	return sum
+  end
+
 end
